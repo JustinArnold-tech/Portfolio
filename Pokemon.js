4e4f570d9456f9d666db7cc4038d6a3f58bcce03
JustinArnold-tech <79416942+JustinArnold-tech@users.noreply.github.com>
2021-08-24 11:10:23 -0600

    loading files

---
+++ Pokemon.js
@@ -0,0 +1,27 @@
+function showSquirtleAlert(){
+    document.getElementById("Header").innerText = "This is the Water starter";
+}
+
+function showBulbasaurAlert(){
+    document.getElementById("Header").innerText = "This is the Grass starter";
+}
+
+function showCharmanderAlert(){
+    document.getElementById("Header").innerText = "This is the Fire starter";
+}
+
+function squirtle(){
+    document.getElementById("pokemonPic").src = "https://static2.gamerantimages.com/wordpress/wp-content/uploads/2021/02/squ-squirtle.jpg";
+    document.getElementById("pokeball1").src = "Popen2.png";
+
+}
+
+function bulbasaur(){
+    document.getElementById("pokemonPic").src = "https://twinfinite.net/wp-content/uploads/2019/11/bulbasaur.jpg";
+    document.getElementById("pokeball2").src = "Popen2.png";
+}
+
+function charmander(){
+    document.getElementById("pokemonPic").src = "https://static.wikia.nocookie.net/pokemon-and-digimon/images/6/63/Ash_Charmander.png/revision/latest?cb=20160113223827";
+    document.getElementById("pokeball3").src = "Popen2.png";
+}
